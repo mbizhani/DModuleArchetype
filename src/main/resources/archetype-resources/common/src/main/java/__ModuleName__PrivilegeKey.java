742736122e0de2252d04969dd362fef6b1225f07
Mehdi Bizhani <mbizhani@gmail.com>
2018-02-08 20:21:48 +0330

    - considering Demeter release 1.0

--- src/main/resources/archetype-resources/common/src/main/java/__ModuleName__PrivilegeKey.java
+++ src/main/resources/archetype-resources/common/src/main/java/__ModuleName__PrivilegeKey.java
@@ -3,9 +3,14 @@ package ${package};
 import org.devocative.demeter.entity.IPrivilegeKey;
 
 public enum ${ModuleName}PrivilegeKey implements IPrivilegeKey {
+	// added as an example for Book entity, they can be reomved!
 	BookAdd, BookEdit;
 
+	// ------------------------------
+
 	private String name;
+
+	// ------------------------------
 
 	@Override
 	public String getName() {
